a77ea9935b54a8be54d74bf337e6f976738cac8d
salaschen <ruowei.chen.aus@gmail.com>
2017-06-04 10:57:42 +1000

    Solution Accepted, time 0.230sec, rank 469

--- UVA/Liu/Chapter5_C++/12096_SetStack/sol.py
+++ UVA/Liu/Chapter5_C++/12096_SetStack/sol.py
@@ -1,15 +1,34 @@
+'''
+update: 
+    04/June/2017: Try to use id to identify set.
+    Store id in self.sets instead of an actual set object.
+'''
+Set2IdMap = {} ; 
+
 class Set:
     def __init__(self):
-        self.depth = 1 ; 
         self.sets = [] ;
+        self.id = -1 ; 
+        key = self.__str__() ; 
+        if key in Set2IdMap:
+            self.id = Set2IdMap[key] ; 
+        else:
+            self.id = len(Set2IdMap) ; 
+            Set2IdMap[key] = self.id ; 
+
+    def updateId(self):
+        self.sets = sorted(self.sets) ;
+        key = self.__str__() ; 
+        if key in Set2IdMap:
+            self.id = Set2IdMap[key] ; 
+        else:
+            self.id = len(Set2IdMap) ; 
+            Set2IdMap[key] = self.id ; 
 
     def __str__(self):
-#        result = 'Card=' + str(self.cardinality())  ;
-#        result += ', Depth=' + str(self.depth) ; 
         result = '(' ; 
-
-        for s in self.sets:
-            result += s.__str__()  + ',' ; 
+        for index in self.sets:
+            result += (str(index) + ",")
         result = result.rstrip(',') ; 
         result += ')' ; 
         return result ; 
@@ -17,17 +36,12 @@ class Set:
     def cardinality(self):
         return len(self.sets) ; 
 
-    def depth(self):
-        return self.depth ; 
-        
-    def Add(self, s2):
-        self.sets.append(s2) ; 
-        if s2.depth >= self.depth:
-            self.depth = s2.depth + 1 ; 
-#        self.sets = sorted(self.sets, key=lambda s: s.depth) ; 
+    def Add(self, s2Id):
+        self.sets.append(s2Id) ; 
         return self; 
     
     def isSame(self, s2):
+        '''
         if self.cardinality() != s2.cardinality():
             return False ; 
         if self.depth == 1 and s2.depth == 1:
@@ -39,70 +53,41 @@ class Set:
             return False;
         # return self.isSameList(self.sets, s2.sets) 
         return True ; 
-
-    def isSameList(l1, l2):
-        while len(l1) > 0:
-            s1 = l1.pop(0) ; 
-            found = False ; 
-            for i in range(0, len(l2)):
-                s2 = l2[i] ; 
-                if s1.depth < s2.depth:
-                    return False ; 
-                if s1.isSame(s2):
-                    l2.pop[i] ; 
-                    found = True ; 
-                    break ; 
-            if found == False:
-                return False ; 
-        return True ; 
-
-    def minus(self, subset):
-        result = Set() ; 
-        selfCopy = Set() ; selfCopy.copy(self) ; 
-        subCopy = Set() ; subCopy.copy(subset) ; 
-        while len(subCopy.sets) > 0:
-            s1 = subCopy.sets.pop() ; 
-            for i in range (0, len(selfCopy.sets)):
-                if s1.isSame(selfCopy.sets[i]):
-                    selfCopy.sets.pop(i) ; 
-                    break ; 
-        for x in selfCopy.sets:
-            result.Add(x) ; 
-        return result ; 
-
+        '''
+        return self.id == s2.id ; 
+    
     # return the list of intersection sets.
     def intersect(self, s2):
-        s1Copy = Set() ; s1Copy.copy(self) ; 
         s2Copy = Set() ; s2Copy.copy(s2) ; 
+        s2Copy.sets = sorted(s2Copy.sets) ; 
 
-        result = Set() ; 
+        inter = Set() ; 
+        minus = Set() ; 
         x = 0 ; 
-        while len(s1Copy.sets) > 0:
-            s = s1Copy.sets.pop() ;
-            for i in range(0, len(s2Copy.sets)):
-                ss = s2Copy.sets[i] ; 
-                if s.isSame(ss) == True:
-                    result.Add(s) ; 
-                    s2Copy.sets.pop(i) ; 
-                    break ; 
-        return result ;   
+        while len(s2Copy.sets) > 0:
+            s = s2Copy.sets.pop(0) ;
+            if self.hasSet(s):
+                inter.Add(s) ; 
+            else:
+                minus.Add(s) ; 
+
+        minus.updateId() ; 
+        return (inter, minus) ; 
 
     def union(self, s2):
-        inter = self.intersect(s2) ; 
-        for x in (s2.minus(inter)).sets:
+        inter, minus = self.intersect(s2) ; 
+        for x in minus.sets:
             self.Add(x) ; 
         return self ; 
 
+    # copy from another set s
     def copy(self, s):
-        self.depth = s.depth ; 
+        self.id = s.id ; 
         for i in s.sets:
             self.Add(i) ; 
 
-    def hasSet(self, s):
-        for ss in self.sets:
-            if ss.isSame(s):
-                return True ; 
-        return False ; 
+    def hasSet(self, sId):
+        return sId in self.sets ; 
 
 def work():
     stack = []
@@ -121,16 +106,20 @@ def work():
             s1 = stack.pop() ; 
             s2 = stack.pop() ; 
             s1.union(s2) ; 
+            s1.updateId() ; 
             stack.append(s1) ; 
         if com == 'INTERSECT':
             s1 = stack.pop() ; 
             s2 = stack.pop() ; 
-            stack.append(s1.intersect(s2)) ; 
+            result = s1.intersect(s2)[0] ; 
+            result.updateId() ; 
+            stack.append(result) ; 
         if com == 'ADD':
             s1 = stack.pop() ; 
             s2 = stack.pop() ; 
-            if s2.hasSet(s1) == False:
-                s2.Add(s1) ; 
+            if s2.hasSet(s1.id) == False:
+                s2.Add(s1.id) ; 
+            s2.updateId() ; 
             stack.append(s2) ; 
         
         result = "" ; 
@@ -139,6 +128,7 @@ def work():
         result = result + str(stack[len(stack)-1].cardinality()) ; 
         print(result) ; 
         
+#    print(Set2IdMap) ; # debug
     print("***") ; 
     return ; 
 
@@ -148,30 +138,34 @@ def test():
     s1.Add(Set()) ; # (())
     s2.Add(Set()) ; # (())
     s1.Add(Set()) ; # ((), ())
+    s1.updateId() ;
     s2.Add(Set()) ; # ((), ()) 
+    s2.updateId() ; 
 
     s3 = Set(); 
     s3.Add(Set()) ; # (())
+    s3.updateId() ; 
     s4 = Set() ; 
     s4.Add(s3) ; # ((()))
+    s4.updateId() ;
+    
     print (s4.hasSet(Set())) ; 
-    s5 = Set() ; 
-    s5.Add(s3) ; 
-    s5.Add(s4) ; 
+    s5 = Set() ;  # ()
+    s5.Add(s3) ;  # ((()))
+    s5.Add(s4) ;  # ((()), ((()))) 
+
     print(s5) ;
     print(s5.hasSet(Set())) ; 
     print(s3.isSame(Set())) ; 
     print(s4.isSame(Set())) ; 
     print(s4) ; 
-    print(s4.cardinality()) ; 
-    print(Set().cardinality()) ; 
-    print(s4.depth) ; 
     print('***') ; 
 
     print(s1) ; 
     print(s2) ; 
     print(s1.isSame(s2)) ; 
-    print(s1.intersect(s2)) ; 
+    print(s1.intersect(s2)[0]) ; 
+    print(s1.intersect(s2)[1]) ; 
     print(s4) ; 
     print(s4.union(s2)) ; 
     print(s4.union(s3)) ; 
@@ -185,7 +179,7 @@ def main():
     x = 0 ;
     T = int(input()) ; 
     for i in range(0, T):
-#        x += 1;
+        x += 1;
         work() ; 
     return  ;
 
