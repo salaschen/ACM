7bb4faa0fa21d76857677591e6cb5722798d4e5d
salaschen <ruowei.chen.aus@gmail.com>
2019-11-03 09:36:02 +1100

    compare with the official implementation

--- Leetcode/128_LongestConsecutiveSequence/sol.py
+++ Leetcode/128_LongestConsecutiveSequence/sol.py
@@ -4,11 +4,33 @@ Level: Hard
 Author: Ruowei Chen
 Note: 
     1) Need to be O(n) complexity.
+    2) AC on the second time, first time missed the empty case.
+    3) Coded the official solution and compare the performance. My implementation does run on O(N) complexity,
+    as shown by the performance test, just on a slower speed than the official implementation.
 '''
 import random ; 
+import time ; 
 
 class Solution:
+    def longestConsecutive2(self, nums:[[int]]) -> int:
+        '''official'''
+        if len(nums) == 0:
+            return 0 ; 
+
+        numSet = set(nums) ; 
+        longestStreak = 1 ; 
+        for num in numSet:
+            if num-1 not in numSet:
+                currentStreak = 1 ; 
+                cur = num ; 
+                while cur+1 in numSet:
+                    currentStreak += 1 ; 
+                    cur += 1 ;
+                longestStreak = max(currentStreak, longestStreak) ; 
+        return longestStreak ; 
+
     def longestConsecutive(self, nums: [[int]]) -> int:
+        '''Personal'''
         length = dict() ;        
         result = 0 ; 
         for i in range(len(nums)):
@@ -42,6 +64,31 @@ class Solution:
         return result ; 
 
 ###### Test #####
+def performanceCompare():
+    s = Solution() ; 
+    m1, m2 = s.longestConsecutive, s.longestConsecutive2 ; 
+    for size in [100,1000,10000,100000,200000,400000]:
+        case = genCase(size) ; 
+        r1, t1 = runCase(m1, case) ; 
+        r2, t2 = runCase(m2, case) ; 
+        ok = 'passed' if r1 == r2 else 'failed' ;
+        print('size {0} {1}: {2} - {3:.3} milliseconds, {4} - {5:.3} milliseconds'.\
+        format(size, ok, m1.__doc__, t1*1000, m2.__doc__, t2*1000 ))
+
+    pass ; 
+
+def runCase(method, case=[]) -> (int, float):
+    startTime = time.time() ; 
+    result = method(case) ; 
+    endTime = time.time() ; 
+    costTime = (endTime-startTime) ;
+    return (result, costTime)
+    
+def genCase(size=100):
+    lst = [random.randint(0, size*2) for x in range(size)]  ; 
+    return lst ; 
+
+
 def test():
     s = Solution() ; 
     l = [100,4,200,1,3,2] ; 
@@ -80,7 +127,9 @@ def case3():
     return (l, expected) ;
 
 def main():
-    test() ; 
+    # test() ; 
+    performanceCompare() ; 
+    return ;
 
 if __name__ == "__main__":
     main() ; 
