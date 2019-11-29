3afcd8174fc12a43fedcabf47730259150f8c43b
salaschen <ruowei.chen.aus@gmail.com>
2019-11-30 10:04:09 +1100

    python 1AC, time: 89.77%, mem: 100%

---
+++ Leetcode/3_LongestSubstring/sol.py
@@ -0,0 +1,22 @@
+'''
+Prob: Medium - 3. Longest SubString without Repeating Characters
+Author: Ruowei Chen
+Date: 30/Nov/2019
+'''
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        result = 0 ; 
+        curString = '' ; 
+        for i in range(len(s)):
+            cur = str[i] ; 
+            if cur in curString:
+                result = max(result, len(curString)) ; 
+                # truncate all the string before the matching character.
+                index = curString.index(cur) ; 
+                curString = curString[index+1:] + cur ; 
+            else:
+                curString += cur ; 
+        result = max(result, len(curString)) ; 
+        return result ; 
+                
+
